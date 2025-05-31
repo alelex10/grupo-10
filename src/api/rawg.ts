a13913ce429f3355a5f2ed79ff92f8a1acf4be72
alelex10 <elvizvida@gmail.com>
2025-05-31 16:31:24 -0300

    feat(catalog): implement CatalogSection and CardImg components

---
+++ src/api/rawg.ts
@@ -0,0 +1,11 @@
+// src/api/rawg.js
+export const API_KEY = "a707fb0dd76f4b69a37f1cb57a7ab2f4";
+export const BASE_URL = "https://api.rawg.io/api";
+
+export async function getGames(params = "") {
+	const response = await fetch(`${BASE_URL}/games?key=${API_KEY}&${params}`);
+	if (!response.ok) {
+		throw new Error("Error al obtener los juegos");
+	}
+	return await response.json();
+}
