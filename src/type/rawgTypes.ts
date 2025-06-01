f648b1d7a158f5516239e15803443d0cb13a5237
alelex10 <elvizvida@gmail.com>
2025-06-01 17:48:40 -0300

    feat: implement game catalog with carousel and card components, update types and API integration

--- src/type/rawgTypes.ts
+++ src/type/rawgTypes.ts
@@ -1,20 +1,21 @@
-interface getGamesResponse {
+export interface getGamesResponse {
     count: number;
     next: string;
     previous: string;
     results: Game[];
 }
 
-interface Game {
+export interface Game {
     id: number;
     name: string;
     slug: string;
     released: string;
     background_image: string;
     rating: number;
+
     // rating_top: number;
     // platforms: Platform[];
-    // genres: Genre[];
+    genres: Genres[];
     // short_screenshots: Screenshot[];
 }
 export interface Genres {
